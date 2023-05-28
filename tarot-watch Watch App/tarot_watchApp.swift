27cec48f266f4c5961c295874afbb71fd34825bc
himanushi <himanushi@users.noreply.github.com>
2023-05-28 09:06:52 +0900

    Initial Commit

---
+++ tarot-watch Watch App/tarot_watchApp.swift
@@ -0,0 +1,17 @@
+//
+//  tarot_watchApp.swift
+//  tarot-watch Watch App
+//
+//  Created by 匿名 on 2023/05/28.
+//
+
+import SwiftUI
+
+@main
+struct tarot_watch_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
