156f7e055f91400202811af0b9f7b22cf4072c42
himanushi <himanushi@users.noreply.github.com>
2023-05-28 19:30:24 +0900

    fix

--- tarot-watch Watch App/ContentView.swift
+++ tarot-watch Watch App/ContentView.swift
@@ -8,14 +8,73 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var tarotDeck = TarotDeck()
+    @State var selectedCard: TarotCard?
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            if let card = selectedCard {
+                Text("\(card.name) - \(card.description)")
+                    .font(.headline)
+                    .padding()
+                Button(action: { selectedCard = nil }) {
+                    Text("Back to deck")
+                }
+            } else {
+                GridView(Array(tarotDeck.cards.indices)) { index in
+                    Button(action: { selectedCard = tarotDeck.cards[index] }) {
+                        Text("🎴")
+                    }
+                    .frame(width: 70, height: 70)
+                }
+            }
         }
-        .padding()
+        .onChange(of: selectedCard) { value in
+            if value == nil {
+                tarotDeck = TarotDeck() // Reshuffle the deck when back to deck
+            }
+        }
+    }
+}
+
+struct GridView<Content: View, ID: Hashable>: View {
+    let data: [ID]
+    let content: (ID) -> Content
+
+    init(_ data: [ID], @ViewBuilder content: @escaping (ID) -> Content) {
+        self.data = data
+        self.content = content
+    }
+
+    var body: some View {
+        let columns: [GridItem] = [.init(.flexible()), .init(.flexible()), .init(.flexible()), .init(.flexible())]
+        LazyVGrid(columns: columns, content: {
+            ForEach(data, id: \.self) { id in
+                content(id)
+            }
+        })
+    }
+}
+
+struct TarotCard: Equatable {
+    var name: String
+    var description: String
+    var isReversed: Bool
+}
+
+struct TarotDeck {
+    var cards: [TarotCard] = []
+    init() {
+        let names = ["The Fool", "The Magician", "The High Priestess", "The Empress", "The Emperor", "The Hierophant", "The Lovers", "The Chariot", "Strength", "The Hermit", "Wheel of Fortune", "Justice", "The Hanged Man", "Death", "Temperance", "The Devil", "The Tower", "The Star", "The Moon", "The Sun", "Judgement", "The World"]
+        let descriptions = ["A new beginning", "Power of manifestation", "Mystery", "Abundance", "Authority", "Wisdom", "Love", "Determination", "Courage", "Introspection", "Luck", "Fairness", "Sacrifice", "Change", "Balance", "Temptation", "Destruction", "Hope", "Fear", "Joy", "Rebirth", "Completion"]
+        var shuffledIndexes = Array(0..<22).shuffled()
+        for _ in 0..<22 {
+            let index = shuffledIndexes.removeLast()
+            let isReversed = Bool.random()
+            let card = TarotCard(name: names[index], description: descriptions[index], isReversed: isReversed)
+            cards.append(card)
+        }
     }
 }
 
